c93fefeb9eb3dbc8354777e89692f71cceab0e5c
ImsirovicAjdin <ajdika@live.com>
2019-07-18 11:43:10 +0200

    Add the make text green / make text black button and it's styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Minimalist</h1>
-  `
+    <div>
+      <div [class.green]="this.green">
+        <h1>Minimalist</h1>
+      </div>
+      <button (click)="toggleThisGreen()">
+        {{ this.green == 1 ? 'Make text black' : 'Make text green' }}
+      </button>
+    </div>
+
+  `,
+  styles: [`
+    .green { color: green }
+  `]
 })
 export class AppComponent  {
-  
+  public green:boolean = false;
+  public big:boolean = false;
+
+  toggleThisGreen() {
+    this.green = !this.green;
+    console.log(this.green);
+  }
 }
