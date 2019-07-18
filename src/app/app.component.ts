17cc577adfbbeb6e7e2c74c3b6f9352df1b939f0
ImsirovicAjdin <ajdika@live.com>
2019-07-18 11:35:43 +0200

    Remove everything that won't be used in this example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'app-root',
+  template: `
+    <h1>Minimalist</h1>
+  `
 })
 export class AppComponent  {
-  name = 'Angular';
+  
 }
